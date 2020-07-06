d6362e26a09d01a5fbcb9c48faa7cf2db7a1a4f2
Arpit Williams <arpitwilliams@yahoo.com>
2020-07-06 11:08:32 +0530

    Added generic project files.

---
+++ Uppy/UppyTests/UppyTests.swift
@@ -0,0 +1,33 @@
+//
+//  UppyTests.swift
+//  UppyTests
+//
+//  Created by Arpit Williams on 30/06/20.
+//  Copyright © 2020 Everli. All rights reserved.
+//
+
+@testable import Uppy
+import XCTest
+
+class UppyTests: XCTestCase {
+
+  override func setUp() {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
+
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    measure {
+      // Put the code you want to measure the time of here.
+    }
+  }
+}
