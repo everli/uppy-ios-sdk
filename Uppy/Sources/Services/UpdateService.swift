abf50e0cd9b4ffce97946f352d5d473aa59a6176
Arpit Williams <arpitwilliams@yahoo.com>
2020-07-10 22:41:51 +0530

    Changed update service to return mock data

--- Uppy/Sources/Services/UpdateService.swift
+++ Uppy/Sources/Services/UpdateService.swift
@@ -15,20 +15,29 @@ class UpdateService {
     Request(endpoint: "/api/v1/updates/iOS/\(appVersion)")
       .sendAsync { response in
 
-        guard let data = response.data, response.isSuccess else {
+        // @Arpit: Remove test data
+        let mockData = "{ \"data\": { \"forced\" : true, \"download_url\" : \"google.com\", \"version\" : \"7.1.0\" } }"
+        guard let data = mockData.data(using: .utf8), let update = try? JSONDecoder().decode(Update.self, from: data) else {
           completionHandler(nil, try? ErrorFactory.decodeError(from: response))
-          logError(response.error)
           return
         }
+        completionHandler(ObjectResponse(data: update), nil)
 
-        do {
-          let updateObject = try JSONDecoder().decode(ObjectResponse<Update>.self, from: data)
-          completionHandler(updateObject, nil)
-        }
-        catch let error as NSError {
-          completionHandler(nil, try? ErrorFactory.decodeError(from: response))
-          logError(error)
-        }
+        // @Arpit: Uncomment code onces api is on staging
+//        guard let data = response.data, response.isSuccess else {
+//          completionHandler(nil, try? ErrorFactory.decodeError(from: response))
+//          logError(response.error)
+//          return
+//        }
+//
+//        do {
+//          let updateObject = try JSONDecoder().decode(ObjectResponse<Update>.self, from: data)
+//          completionHandler(updateObject, nil)
+//        }
+//        catch let error as NSError {
+//          completionHandler(nil, try? ErrorFactory.decodeError(from: response))
+//          logError(error)
+//        }
       }
   }
 }
