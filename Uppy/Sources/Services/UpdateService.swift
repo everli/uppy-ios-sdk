83a9ef3bba8966dd450eda2b2d2b905f604c0709
Arpit Williams <arpitwilliams@yahoo.com>
2020-07-23 17:14:17 +0530

    Removed redundant code for mock data

--- Uppy/Sources/Services/UpdateService.swift
+++ Uppy/Sources/Services/UpdateService.swift
@@ -12,29 +12,23 @@ class UpdateService {
 
   func checkUpdates(for appVersion: String, with completionHandler: @escaping (ObjectResponse<Update>?, ErrorResponse?) -> Void) {
 
-    // @Arpit: Remove test data
-    let mockData = "{ \"forced\" : true, \"download_url\" : \"https://www.google.com/\", \"version\" : \"1.0.0\" }"
-    guard let data = mockData.data(using: .utf8), let update = try? JSONDecoder().decode(Update.self, from: data) else { return }
-    completionHandler(ObjectResponse(data: update), nil)
+    Request(endpoint: "/api/v1/updates/iOS/\(appVersion)")
+      .sendAsync { response in
 
-    // @Arpit: Uncomment code onces api is on staging
-//    Request(endpoint: "/api/v1/updates/iOS/\(appVersion)")
-//      .sendAsync { response in
-//
-//        guard let data = response.data, response.isSuccess else {
-//          completionHandler(nil, try? ErrorFactory.decodeError(from: response))
-//          logError(response.error)
-//          return
-//        }
-//
-//        do {
-//          let updateObject = try JSONDecoder().decode(ObjectResponse<Update>.self, from: data)
-//          completionHandler(updateObject, nil)
-//        }
-//        catch let error as NSError {
-//          completionHandler(nil, try? ErrorFactory.decodeError(from: response))
-//          logError(error)
-//        }
-//      }
+        guard let data = response.data, response.isSuccess else {
+          completionHandler(nil, try? ErrorFactory.decodeError(from: response))
+          logError(response.error)
+          return
+        }
+
+        do {
+          let updateObject = try JSONDecoder().decode(ObjectResponse<Update>.self, from: data)
+          completionHandler(updateObject, nil)
+        }
+        catch let error as NSError {
+          completionHandler(nil, try? ErrorFactory.decodeError(from: response))
+          logError(error)
+        }
+      }
   }
 }
